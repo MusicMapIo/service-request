aca838c465ea3a4f701315f09d4b50a70eafa4d7
Wes Todd <wes@wesleytodd.com>
2015-10-04 14:06:09 -0500

    better logging of errors

--- index.js
+++ index.js
@@ -41,30 +41,40 @@ var serviceRequest = module.exports = function(opts, done) {
 
 	// Make the actual request
 	request(opts, function(err, resp, body) {
-		// Log error responses
-		if (err) {
-			logger.error('Service request error', {
-				error: err, 
-				options: opts,
-				response: resp,
-				body: body
-			});
-		} else if (resp.statusCode >= 500) {
-			logger.error('Service request 500 response', {
-				options: opts,
-				response: resp,
-				body: body
-			});
-		} else if (resp.statusCode >= 400) {
-			logger.warning('Service request 400 response', {
-				options: opts,
-				response: resp,
-				body: body
-			});
+		if (err || resp.statusCode >= 400) {
+			serviceRequest.logErrorResponse(opts, err, resp, body);
 		}
+
 		done(err, resp, body);
 	});
 
+};
+
+serviceRequest.logErrorResponse = function(opts, err, resp, body) {
+	var msg = 'Service request error',
+		level = 'error',
+		meta = {
+			options: opts,
+			response: {
+				code: resp.statusCode,
+				headers: resp.headers
+			}
+		};
+
+	// Log error responses
+	if (err) {
+		meta.error = err;
+	} else if (resp.statusCode >= 500) {
+		meta.body = body;
+		body && body.errors ? meta.errors = body.errors : false;
+		msg = 'Service request 500 response';
+	} else if (resp.statusCode >= 400) {
+		meta.body = body;
+		body && body.errors ? meta.errors = body.errors : false;
+		level = 'warning'
+		msg = 'Service request 400 response';
+	}
+	logger[level](msg, meta);
 };
 
 serviceRequest.hostname = 'localhost';
